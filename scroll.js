806ec5df9f1c7d21e87ed74d4d475f2cc8cfd4bb
MLsanta <MLsanta527@gmail.com>
2025-09-25 16:34:58 +0900

    .

--- scroll.js
+++ scroll.js
@@ -1,18 +1,25 @@
-let texts = document.getElementById("texts");
-let banner = document.querySelector(".banner");
-function startScroll() {
-    let sy = window.scrollY;
-    console.log("scrollY:", sy);
-    if (sy > 100) {
-        texts.classList.add("active");
-    } else {
-        texts.classList.remove("active");
+document.addEventListener("DOMContentLoaded", () => {
+    let texts = document.getElementById("texts");
+    let banner = document.querySelector(".banner");
+    let scrolls = document.querySelector(".scrolls");
+
+    function startScroll() {
+        let sy = window.scrollY;
+        console.log("scrollY:", sy);
+        if (sy > 100) {
+            texts.classList.add("active");
+            scrolls.classList.add("off");
+        } else {
+            texts.classList.remove("active");
+            scrolls.classList.remove("off");
+        }
+        if (sy > 440) {
+            banner.classList.add("on");
+        } else {
+            banner.classList.remove("on");
+        }
     }
-    if (sy > 440) {
-        banner.classList.add("on");
-    } else {
-        banner.classList.remove("on");
-    }
-}
-startScroll();
-window.addEventListener("scroll", startScroll);
+
+    startScroll();
+    window.addEventListener("scroll", startScroll);
+});
